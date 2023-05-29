69160031a6697a01613186a45f03e3d6a9ae4fcb
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-05-29 18:09:46 +0100

    feat(Personaje,paquetes basicos): Se ha implementado la clase Personaje y la estructura básica de paquetes

---
+++ src/net/salesianos/lacuesta/Personaje.java
@@ -0,0 +1,14 @@
+package net.salesianos.lacuesta;
+
+public class Personaje {
+    private int salud;
+    private int defensa;
+    private int fuerza;
+
+
+    public Personaje(int defensa, int fuerza) {
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+        this.salud=12;
+    }
+}
