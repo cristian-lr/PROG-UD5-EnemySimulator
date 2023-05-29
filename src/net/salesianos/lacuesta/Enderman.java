d53fe6621ba5fc3d83a329a3f602e46da3a9a0aa
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-05-29 21:35:36 +0100

    feat(Enderman):Implementada la clase Enderman

---
+++ src/net/salesianos/lacuesta/Enderman.java
@@ -0,0 +1,11 @@
+package net.salesianos.lacuesta;
+
+public class Enderman extends MobHostil {
+    public Enderman() {
+        super(14, 6);
+    }
+
+    public void moverse() {
+        System.out.println("Se teletransporta detrás");
+    }
+}
