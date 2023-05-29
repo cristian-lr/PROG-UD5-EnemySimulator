f7d9958957ffe2024f0bd2bac5bc81f2ce3dfdeb
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-05-29 18:28:46 +0100

    feat(Mob):Implementada la clase Mob

---
+++ src/net/salesianos/lacuesta/Mob.java
@@ -0,0 +1,11 @@
+package net.salesianos.lacuesta;
+
+public abstract class Mob {
+    private int salud;
+
+    public Mob(int salud) {
+        this.salud = salud;
+    }
+
+    public abstract void moverse();
+}
