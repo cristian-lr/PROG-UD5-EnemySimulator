ac66ee6d5b6c4bae6de379ea5ea12d5065db87e6
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-05-29 19:34:59 +0100

    feat(Personaje):Se ha implementado los métodos de la interfaz

--- src/net/salesianos/lacuesta/Personaje.java
+++ src/net/salesianos/lacuesta/Personaje.java
@@ -1,14 +1,23 @@
 package net.salesianos.lacuesta;
 
-public class Personaje {
+public class Personaje implements Combate{
     private int salud;
     private int defensa;
     private int fuerza;
-
 
     public Personaje(int defensa, int fuerza) {
         this.defensa = defensa;
         this.fuerza = fuerza;
         this.salud=12;
+    }
+
+    public int atacar(){
+        return fuerza;
+    }
+
+
+    public void recibirAtaque(int ataque) {
+        salud-=(ataque-defensa);
+        System.out.println("El daño recibido ha sido de "+ataque+" y la salud restante es "+salud);
     }
 }
